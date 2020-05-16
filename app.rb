c613f063a2a3d18366db5d34d1b28943befca097
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-05-16 17:04:06 +0500

    Add Rakefile,update Gemmgile,add model

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 
+set :database, {adapter: 'sqlite3', database: 'pizzashop.sqlite'}
+
+class Product < ActiveRecord::Base
+
+end
 
 get '/' do 
 
