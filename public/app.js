4f2b2b08100ac6a3fa10b8d6f8fd356d4d0d565b
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-05-16 16:29:01 +0500

    update function app.js

--- public/app.js
+++ public/app.js
@@ -4,8 +4,8 @@ function something()
 {
   var x = window.localStorage.getItem('bbb')
 
-  x = x * 1 + 1 
- window.localStorage.setItem('bbb', x)
+  //x = x * 1 + 1 
+ window.localStorage.setItem('bbb', x++)
 	alert(x)
 	
 }	
