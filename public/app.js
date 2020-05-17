8ec338ca0423b77139eace494b73f60138a56bb2
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-05-17 16:01:07 +0500

    update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -12,6 +12,11 @@ function something()
 
 function add_to_card(id)
 {
-   alert("with added pizza with id:" + id )
+	var key = 'product_' + id
+	
+   var x = window.localStorage.getItem(key)
+
+   x = x * 1 + 1
+   window.localStorage.setItem(key , x)
 }
 
