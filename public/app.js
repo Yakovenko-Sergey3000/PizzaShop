b03d25e0873e08f936d22bb21f698b880985e5eb
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-05-26 16:06:37 +0500

    add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ function something()
 	
 }	
 
-function add_to_card(id)
+function add_to_cart(id)
 {
 	var key = 'product_' + id
 	
@@ -18,5 +18,25 @@ function add_to_card(id)
 
    x = x * 1 + 1
    window.localStorage.setItem(key , x)
+
+   alert('Items in your cart ' + cart_get_number_of_items())
+
 }
 
+
+function cart_get_number_of_items()
+{
+	var cnt = 0
+
+	for (var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key)
+
+		if(key.indexOf('product_') == 0)  // метод которые показывает как должен нызваться ключь
+ 		{
+			cnt = cnt + value * 1
+		}
+	}
+	return cnt
+}
